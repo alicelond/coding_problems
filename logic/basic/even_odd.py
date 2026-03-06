1b7250ecc2a8b57ef08a30ced1129c3e51d7ca1f
alicelond <alice.londero@gmail.com>
2026-03-06 20:52:25 -0300

    add even_odd problem

--- logic/basic/even_odd.py
+++ logic/basic/even_odd.py
@@ -1,4 +1,19 @@
+import unittest
 # Given a number n, check whether it is even or odd. 
 # Return true for even and false for odd.
 
 def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+class TestIsEven(unittest.TestCase):
+    # Test cases for the function is_even
+    def test_even_number(self):
+        self.assertTrue(is_even(4))
+    def test_odd_number(self):
+        self.assertFalse(is_even(5))
+    
+if __name__ == "__main__":
+    unittest.main()
